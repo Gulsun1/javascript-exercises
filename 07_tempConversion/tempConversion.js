8d59f6238295c816d00aeab6b95e5f654efc0206
alexc <166944852+Gulsun1@users.noreply.github.com>
2024-07-13 21:38:19 +0200

    Partially complete calculator exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,10 @@
 const convertToCelsius = function(fahrenheit) {
-  let celsius = Number(((fahrenheit - 32) * (5/9)).toFixed(2));
+  let celsius = Number(((fahrenheit - 32) * (5/9)).toFixed(1));
   return celsius;
 };
 
 const convertToFahrenheit = function(celsius) {
-  let fahrenheit = Number((celsius * (9/5) + 32).toFixed(2));
+  let fahrenheit = Number((celsius * (9/5) + 32).toFixed(1));
   return fahrenheit;
 };
 
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(x,y) {
+	let solution = x + y;
+  return solution;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	let solution = x - y;
+  return solution;
 };
 
-const sum = function() {
-	
+const sum = function(...x) { //Pending fix
+	let result = x.reduce((accumulator, currentValue) => {
+      return accumulator + currentValue;
+  }, 0);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(...x) { //Pending fix
+  let result = x.reduce((accumulator, currentValue) => {
+    return accumulator * currentValue;
+}, 0);
+return result;
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	let result = x ** y;
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let factorial = 1;  
+  for (let i = x; i > 0; i--) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
