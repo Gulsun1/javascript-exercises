47dee15c9565140781cd0b339c024a9f0e2e8320
alexc <166944852+Gulsun1@users.noreply.github.com>
2024-07-13 20:27:47 +0200

    Complete first 4 exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversed = "";
+    let length = string.length - 1;
+    if (string.length == 0) {
+        return "";
+    } else {
+        for (i = length; i >= 0; i--) {
+            reversed += string[i];
+        }
+        return reversed;
+    }
 };
+
+reverseString("Hello");
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elementToRemove) {
+   elementToRemove.forEach(elementToRemove => {
+        let index  = array.indexOf(elementToRemove);
+        while (index > -1) {
+            array.splice(index, 1);
+            index = array.indexOf(elementToRemove);
+        }
+   });
+   return array;
 };
 
 // Do not edit below this line
