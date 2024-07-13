dac3bab8c90764518adef3e5576d33f47a41b16b
Alex <166944852+Gulsun1@users.noreply.github.com>
2024-07-13 10:11:12 +0200

    sumALL completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber,lastNumber) {
+    let sum = 0;
+    if (firstNumber < 0 || lastNumber < 0 || typeof firstNumber !== "number" || typeof lastNumber !== "number") {
+      sum = "ERROR";
+    } else if (firstNumber > lastNumber) {
+      for (let i = firstNumber; i >= lastNumber; i--) {
+        sum += i;
+      }
+    } else if (firstNumber < lastNumber) {
+      for (let i = firstNumber; i <= lastNumber; i++) {
+          sum += i;
+      }
+    }
+    return sum;
 };
 
 // Do not edit below this line
